a36ce952545b4f2d3bd5833918c45729e655f4ff
Mario Schneider <mario.schneider04@gmail.com>
2018-11-23 16:16:52 +0100

    Proto-Design

--- Bordgame Digitiser/Board.h
+++ Bordgame Digitiser/Board.h
@@ -54,6 +54,10 @@ namespace BordgameDigitiser {
 	private: System::Windows::Forms::ListBox^  gameLog;
 	private: System::Windows::Forms::Panel^  panel2;
 
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
+
+
 
 
 
@@ -73,26 +77,33 @@ namespace BordgameDigitiser {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Board::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->panel3 = (gcnew System::Windows::Forms::Panel());
 			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
 			this->gameLog = (gcnew System::Windows::Forms::ListBox());
 			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1->SuspendLayout();
 			this->panel2->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
-			this->panel1->Location = System::Drawing::Point(21, 23);
+			this->panel1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel1.BackgroundImage")));
+			this->panel1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Location = System::Drawing::Point(20, 20);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(960, 960);
+			this->panel1->Size = System::Drawing::Size(1920, 1920);
 			this->panel1->TabIndex = 0;
 			// 
 			// panel3
 			// 
-			this->panel3->Location = System::Drawing::Point(21, 1008);
+			this->panel3->Location = System::Drawing::Point(1980, 1490);
 			this->panel3->Name = L"panel3";
-			this->panel3->Size = System::Drawing::Size(959, 295);
+			this->panel3->Size = System::Drawing::Size(1240, 450);
 			this->panel3->TabIndex = 2;
 			// 
 			// gameLog
@@ -104,23 +115,34 @@ namespace BordgameDigitiser {
 			this->gameLog->Location = System::Drawing::Point(0, 0);
 			this->gameLog->Name = L"gameLog";
 			this->gameLog->ScrollAlwaysVisible = true;
-			this->gameLog->Size = System::Drawing::Size(789, 1275);
+			this->gameLog->Size = System::Drawing::Size(1240, 1430);
 			this->gameLog->TabIndex = 0;
 			// 
 			// panel2
 			// 
 			this->panel2->Controls->Add(this->gameLog);
-			this->panel2->Location = System::Drawing::Point(1012, 22);
+			this->panel2->Location = System::Drawing::Point(1980, 20);
 			this->panel2->Name = L"panel2";
-			this->panel2->Size = System::Drawing::Size(789, 1281);
+			this->panel2->Size = System::Drawing::Size(1240, 1430);
 			this->panel2->TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->pictureBox1->Location = System::Drawing::Point(240, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(240, 240);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
 			// 
 			// Board
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->AutoSize = true;
-			this->ClientSize = System::Drawing::Size(1825, 1343);
+			this->ClientSize = System::Drawing::Size(2574, 2089);
 			this->Controls->Add(this->panel3);
 			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->panel1);
@@ -129,7 +151,10 @@ namespace BordgameDigitiser {
 			this->Name = L"Board";
 			this->Padding = System::Windows::Forms::Padding(20);
 			this->Text = L"Board";
+			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
+			this->panel1->ResumeLayout(false);
 			this->panel2->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 
 		}
