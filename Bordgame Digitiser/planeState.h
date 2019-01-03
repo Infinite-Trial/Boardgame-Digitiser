338b37f067d777c736dc8f246ebe75d26face652
Infinite-Trial <33350846+Infinite-Trial@users.noreply.github.com>
2019-01-03 16:27:07 +0100

    Planestate renamed

--- Bordgame Digitiser/planeState.h
+++ Bordgame Digitiser/planeState.h
@@ -1,22 +1,26 @@
+//Author: Gross
+//Discription: Helps to savely handle a 2D vector to store a boardstate
+
 #pragma once
 #include "opencv2/core/types.hpp"
 #include "CONSTANTS.h"
 #include <vector>
-class planeState
+
+class PlaneState
 {
 public:
-	planeState();
-	~planeState();
+	PlaneState();
+	~PlaneState();
 	//getter
 	pieceTypes getPieceAt(int x,int y);
 	pieceTypes getPieceAt(cv::Point2i pos);
 	//setter
-	void setPieceAt(chessCoord x, int y);
-	void setPieceAt(cv::Point2i pos);
+	void setPieceAt(int x, int y, pieceTypes dif);
+	void setPieceAt(cv::Point2i pos, pieceTypes dif);
 	//methods
-	std::vector<cv::Point2i> difference(const planeState reference); //returns the coordinates of the different fields
-	//operator
-	bool equals(const planeState& reference);
+	std::vector<cv::Point2i> difference(const PlaneState reference); //returns the coordinates of the different fields
+	bool equals(PlaneState reference); //compares two boards
+	void reset(); //resets the board
 private:
 	std::vector<std::vector<pieceTypes>> state;
 };
