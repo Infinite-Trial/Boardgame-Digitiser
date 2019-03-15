2aac95c2485400a979327831bc0d3b3a40e25609
Mario Schneider <mario.schneider04@gmail.com>
2019-03-15 11:20:23 +0100

    Rules edited

--- Bordgame Digitiser/Rules.cpp
+++ Bordgame Digitiser/Rules.cpp
@@ -1,8 +1,14 @@
 #include "Rules.h"
 
-bool whitePawn(PlaneState oldState, PlaneState newState)
+bool whitePawn(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference)
 {
-
+	cv::Point2i oldPoint, newPoint;
+	if (oldState.getPieceAt(difference[0]) == WP) {
+		oldPoint = difference[0];
+	}
+	else {
+		newPoint = difference[0];
+	}
 	return false;
 }
 
@@ -11,21 +17,37 @@ bool check(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> di
 	return false;
 }
 
+pieceTypes dedectMovedPiece(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference)
+{
+
+	if (oldState.getPieceAt(difference[0]) == newState.getPieceAt(difference[1])) {
+
+		return newState.getPieceAt(difference[1]);
+	}
+
+	if (oldState.getPieceAt(difference[1]) == newState.getPieceAt(difference[0])) {
+
+		return newState.getPieceAt(difference[0]);
+	}
+
+}
+
 void rulecheck(PlaneState oldState, PlaneState newState, pieceTypes type, cv::Point2i enPassant_var, std::vector<cv::Point2i> difference)
 {
 	switch (difference.size()) {
-	case 2: 
-		promotion(newState, oldState, difference);
-		selection(newState, oldState, captureOrMove(newState, oldState, difference), enPassant_var, difference);		
+	case 2:
+		rulebreak(promotion(newState, oldState, difference));
+		rulebreak(selection(newState, oldState, dedectMovedPiece(newState, oldState, difference), enPassant_var, difference));
 		break;
 	case 3:
-		enPassant(newState, oldState, enPassant_var, difference);
+		rulebreak(enPassant(newState, oldState, enPassant_var, difference));
 		break;
 	case 4:
-		casteling(newState, oldState, difference);
+		rulebreak(casteling(newState, oldState, difference));
 		break;
 	default:
-		rulebreak();
-		break
+		rulebreak(true);
+		break;
 	}
 }
+
--- Bordgame Digitiser/Rules.h
+++ Bordgame Digitiser/Rules.h
@@ -3,7 +3,7 @@
 #include "CONSTANTS.h"
 #include "opencv2/core.hpp"
 
-bool whitePawn(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference);
+bool whitePawn(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference, pieceType );
 bool blackPawn(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference);
 bool king(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference);
 bool queen(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference);
@@ -14,10 +14,10 @@ bool promotion(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i
 bool enPassant(PlaneState oldState, PlaneState newState, cv::Point2i enPassant_var, std::vector<cv::Point2i> difference);
 bool casteling(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference);
 bool check(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference);
-pieceTypes captureOrMove(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference);
+pieceTypes dedectMovedPiece(PlaneState oldState, PlaneState newState, std::vector<cv::Point2i> difference);
 void rulecheck(PlaneState oldState, PlaneState newState, pieceTypes type, cv::Point2i enPassant_var, std::vector<cv::Point2i> difference);
 
-void selection(PlaneState oldState, PlaneState newState, pieceTypes type, cv::Point2i enPassant_var, std::vector<cv::Point2i> difference);
-void rulebreak();
+bool selection(PlaneState oldState, PlaneState newState, pieceTypes type, cv::Point2i enPassant_var, std::vector<cv::Point2i> difference);
+void rulebreak(bool inOrder);
 
 
